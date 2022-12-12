ac9a7b41ea051d413a73840f97c68fb8c436f848
Naishadh Parmar <naishadh_parmar@live.com>
2022-12-12 00:55:27 -0500

    Add crime data cleaning and manipulation

---
+++ data_collection/1999-2002.R
@@ -0,0 +1,61 @@
+
+getdata.1999 <- function(crimes_links, year) {
+  library(httr)
+  tmp <- tempfile(fileext = '.xls')
+  writeBin(GET(crimes_links[crimes_links['Year'] == year, 'Link'])$content, tmp)
+  crimes <- read_excel(tmp)
+  rows_to_remove <- c()
+  for(i in 1:nrow(crimes)) {
+    if(sum(is.na(crimes[i, ])) >= ncol(crimes)-1) rows_to_remove <- append(rows_to_remove, i)
+    else break
+  }
+  for(i in nrow(crimes):1) {
+    if(is.na(crimes[i, 1]) | crimes[i, 1] != 'Rate per 100,000 inhabitants') rows_to_remove <- append(rows_to_remove, i)
+    else break
+  }
+  crimes <- crimes[-rows_to_remove, ]
+  cols_to_remove <- c()
+  for(i in 1:ncol(crimes)) {
+    if(is.na(crimes[1, i])) cols_to_remove <- append(cols_to_remove, i)
+  }
+  crimes <- crimes[-cols_to_remove]
+  colnames(crimes) <- crimes[1, ]
+  crimes <- crimes[-1, ]
+  crimes_condensed <- data.frame(Year=c(), State=c(), Area=c(), Population=c(),
+                                 Violent=c(), Property=c(), Murder=c(),
+                                 Rape=c(), Robbery=c(), Assault=c(),
+                                 Burglary=c(), Theft=c(), Motor=c(), Arson=c())
+  new_state <- ''
+  i <- 1
+  while(i <= nrow(crimes)) {
+    if(new_state == '') {
+      i = i+1
+      new_state <- gsub("[0-9]*", "", crimes[i, 1])
+      i = i+2
+    }
+    else {
+      area <- gsub("[0-9]*", "", crimes[i, 1])
+      where <- 0
+      if(grepl('Total', area, fixed=TRUE)) where <- i
+      else if(grepl('Estimated total', crimes[i+2, 1], fixed=TRUE)) where <- i+2
+      else if(grepl('Area actually reporting', crimes[i+1, 1], fixed=TRUE)) where <- i+1
+      else where <- i
+      if(!is.na(crimes[i, 2]) & crimes[i, 2] != 'None') {
+        row <- data.frame(Year=c(year), State=c(new_state), Area=c(area), Population=c(crimes[i, 2]),
+                          Violent=c(crimes[where, 5]), Property=c(crimes[where, 6]), Murder=c(crimes[where, 7]),
+                          Rape=c(crimes[where, 8]), Robbery=c(crimes[where, 9]), Assault=c(crimes[where, 10]),
+                          Burglary=c(crimes[where, 11]), Theft=c(crimes[where, 12]), Motor=c(crimes[where, 13]),
+                          Arson=c(crimes[where, 14]))
+        crimes_condensed <- rbind(crimes_condensed, row)
+      }
+      if(grepl('Total', area, fixed=TRUE)) {
+        i = where+2
+        new_state = ''
+      }
+      else i = where+1
+    }
+  }
+  colnames(crimes_condensed) <- c('Year', 'State', 'Area', 'Population', 'Violent', 'Property',
+                                  'Murder', 'Rape', 'Robbery', 'Assault', 'Burglary', 'Theft', 'Motor', 'Arson')
+  return(crimes_condensed)
+}
---
+++ data_collection/2013-2016.R
@@ -0,0 +1,51 @@
+getdata.2013 <- function(crimes_links, year) {
+  library(httr)
+  tmp <- tempfile(fileext = '.xls')
+  writeBin(GET(crimes_links[crimes_links['Year'] == year, 'Link'])$content, tmp)
+  crimes <- read_excel(tmp)
+  rows_to_remove <- c(1,2)
+  for(i in nrow(crimes):1) {
+    if(!is.na(crimes[i, 1])) rows_to_remove <- append(rows_to_remove, i)
+    else break
+  }
+  crimes <- crimes[-rows_to_remove, ]
+  cols_to_remove <- c()
+  for(i in 1:ncol(crimes)) {
+    if(is.na(crimes[1, i])) cols_to_remove <- append(cols_to_remove, i)
+  }
+  crimes <- crimes[-cols_to_remove]
+  colnames(crimes) <- crimes[1, ]
+  crimes <- crimes[-1, ]
+  crimes_condensed <- data.frame(Year=c(), State=c(), Area=c(), Population=c(),
+                                      Violent=c(), Property=c(), Murder=c(),
+                                      Rape=c(), Robbery=c(), Assault=c(),
+                                      Burglary=c(), Theft=c(), Motor=c(), Arson=c())
+  new_state <- ''
+  i <- 1
+  while(i <= nrow(crimes)) {
+    if(new_state == '') new_state <- gsub("[0-9]*", "", crimes[i, 1])
+    else {
+      area <- gsub("[0-9]*", "", crimes[i, 2])
+      where <- 0
+      if(grepl('Total', area, fixed=TRUE)) where <- i
+      else if(!is.na(crimes[i+2, 3]) & crimes[i+2, 3] == 1) where <- i+2
+      else if(!is.na(crimes[i+1, 3]) & crimes[i+1, 3] == 1) where <- i+1
+      else where <- i
+      if(!is.na(crimes[i, 3]) & crimes[i, 3] != 'None') {
+        row <- data.frame(Year=c(year), State=c(new_state), Area=c(area), Population=c(crimes[i, 3]),
+                          Violent=c(crimes[where, 4]), Property=c(crimes[where, 10]), Murder=c(crimes[where, 5]),
+                          Rape=c(crimes[where, 6]), Robbery=c(crimes[where, 8]), Assault=c(crimes[where, 9]),
+                          Burglary=c(crimes[where, 11]), Theft=c(crimes[where, 12]), Motor=c(crimes[where, 13]))
+        crimes_condensed <- rbind(crimes_condensed, row)
+      }
+      if(grepl('Total', area, fixed=TRUE)) {
+        i = where+2
+        new_state = ''
+      }
+      else i = where+1
+    }
+  }
+  colnames(crimes_condensed) <- c('Year', 'State', 'Area', 'Population', 'Violent', 'Property',
+                                  'Murder', 'Rape', 'Robbery', 'Assault', 'Burglary', 'Theft', 'Motor')
+  return(crimes_condensed)
+}
